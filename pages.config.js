3e2b20c57f7b2b759488707e2f8e94de1bd3600d
Vitali <63733022+digitalbutlers@users.noreply.github.com>
2023-03-27 11:15:31 +0300

    upd: Change name files and class, formatting image to .webp (Our company to Overview))

--- pages.config.js
+++ pages.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 
 const pages = [
     {name: 'main', path: resolve(__dirname, './src/index.html')},
-    {name: 'our-company', path: resolve(__dirname, './src/our-company.html')},
+    {name: 'overview', path: resolve(__dirname, './src/overview.html')},
 ];
 
 export default pages
