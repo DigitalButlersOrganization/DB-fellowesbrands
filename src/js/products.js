86da7a3dc9f9727ec4738f30e503001d34e6231a
Victor Kasap <kasap.victor@gmail.com>
2023-03-28 11:44:30 +0300

    feat: Added Story hero page

--- src/js/products.js
+++ src/js/products.js
@@ -1,8 +1,7 @@
-
 const initVideo = () => {
-  const video = document.querySelector("#products-work__video");
-  const videoBTN = document.querySelector(".products-work__video-button");
-  
+  const video = document.querySelector('#products-work__video');
+  const videoBTN = document.querySelector('.products-work__video-button');
+
   video.addEventListener('click', () => {
     video.paused ? video.play() : video.pause();
     videoBTN.classList.toggle('js--paused');
