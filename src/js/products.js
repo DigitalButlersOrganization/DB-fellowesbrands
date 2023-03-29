450df264acee29be595c2e67da74ca68b9b60017
Vitali <63733022+digitalbutlers@users.noreply.github.com>
2023-03-29 11:58:29 +0300

    feat: Add process section (Products page)

--- src/js/products.js
+++ src/js/products.js
@@ -2,7 +2,7 @@
 const initVideo = () => {
   const video = document.querySelector("#products-work__video");
   const videoBTN = document.querySelector(".products-work__video-button");
-  
+
   video.addEventListener('click', () => {
     video.paused ? video.play() : video.pause();
     videoBTN.classList.toggle('js--paused');
@@ -10,3 +10,32 @@ const initVideo = () => {
 };
 
 initVideo();
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const slidesAction = document.querySelectorAll('.products-process__accordion-item');
+
+  slidesAction.forEach(slideAction => slideAction.addEventListener('click', (e) => {
+    document.querySelector('.products-process__accordion-item.js--active').classList.remove("js--active");
+    slideAction.classList.add("js--active");
+  }));
+});
+
+const checkWidth = function() {
+  const windowsize = document.width();
+  if (windowsize > 480) {
+      const slideWidth = document.querySelector('.js--active').width();
+      document.querySelector('.products-process__accordion-item-content').css({
+          width: `${slideWidth}px`,
+      });
+  }
+
+}
+
+document.addEventListener("resize", () => {
+  checkWidth();
+
+  clearTimeout(window.resizedFinished);
+  window.resizedFinished = setTimeout( checkWidth , 500);
+});
